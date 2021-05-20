8f07c66b08955fa69f57dffc4dc197e0f6bbc5bb
ayushi0014 <ayushi0082.cse19@chitkara.edu.in>
2021-05-20 23:29:31 +0530

    reused cards components

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,34 +1,54 @@
-import logo from './MaindivComponents/RightContainer/symptoms.png'
-const Cards=[{
-    til:"COUGHING",
-    imgsrc:{logo},
-    slink:"https://www.youtube.com/watch?v=FEf412bSPLs",
-},
-{
-    til:"SORE THROAT",
-    imgsrc:"https://uploads.dailydot.com/2020/08/avengers-5.jpg?fm=pjpg&ixlib=php-3.3.0",
-    slink:"https://www.youtube.com/watch?v=FEf412bSPLs",
-},
-{
-    til:"SHORTHNESS OF BREATH",
-    imgsrc:"https://iwall365.com/iPhoneWallpaper/640x960/1310/Iron-Man-3-Robert-Downey-Jr_640x960_iPhone_4_wallpaper.jpg",
-    slink:"https://www.youtube.com/watch?v=FEf412bSPLs",
-},
-{
-    til:"LOTS OF PHLEGM",
-    imgsrc:"https://iwall365.com/iPhoneWallpaper/640x960/1310/Iron-Man-3-Robert-Downey-Jr_640x960_iPhone_4_wallpaper.jpg",
-    slink:"https://www.youtube.com/watch?v=FEf412bSPLs",
-},
-{
-    til:"FEVER",
-    imgsrc:"https://iwall365.com/iPhoneWallpaper/640x960/1310/Iron-Man-3-Robert-Downey-Jr_640x960_iPhone_4_wallpaper.jpg",
-    slink:"https://www.youtube.com/watch?v=FEf412bSPLs",
-},
-{
-    til:"FATIGUE",
-    imgsrc:"https://iwall365.com/iPhoneWallpaper/640x960/1310/Iron-Man-3-Robert-Downey-Jr_640x960_iPhone_4_wallpaper.jpg",
-    slink:"https://www.youtube.com/watch?v=FEf412bSPLs",
-},
-];
+import React, {useEffect, useRef} from 'react'
+import './Symptom.css'
 
-export default Cards;
+    
+
+function Cards({ src, heading, desc }) {
+
+    return (
+        
+        <div className="card">
+        <div className="card_info">
+            <img  src={src} className="card_img"/>
+                <h3 className="card_title">{heading}</h3>
+                <p className="card_explanation">{desc}</p>
+            </div>
+    </div>
+
+    )
+}
+
+export default Cards
+
+// const Cards=[{
+//     til:"COUGHING",
+//     imgsrc:{logo},
+//     slink:"https://www.youtube.com/watch?v=FEf412bSPLs",
+// },
+// {
+//     til:"SORE THROAT",
+//     imgsrc:"https://uploads.dailydot.com/2020/08/avengers-5.jpg?fm=pjpg&ixlib=php-3.3.0",
+//     slink:"https://www.youtube.com/watch?v=FEf412bSPLs",
+// },
+// {
+//     til:"SHORTHNESS OF BREATH",
+//     imgsrc:"https://iwall365.com/iPhoneWallpaper/640x960/1310/Iron-Man-3-Robert-Downey-Jr_640x960_iPhone_4_wallpaper.jpg",
+//     slink:"https://www.youtube.com/watch?v=FEf412bSPLs",
+// },
+// {
+//     til:"LOTS OF PHLEGM",
+//     imgsrc:"https://iwall365.com/iPhoneWallpaper/640x960/1310/Iron-Man-3-Robert-Downey-Jr_640x960_iPhone_4_wallpaper.jpg",
+//     slink:"https://www.youtube.com/watch?v=FEf412bSPLs",
+// },
+// {
+//     til:"FEVER",
+//     imgsrc:"https://iwall365.com/iPhoneWallpaper/640x960/1310/Iron-Man-3-Robert-Downey-Jr_640x960_iPhone_4_wallpaper.jpg",
+//     slink:"https://www.youtube.com/watch?v=FEf412bSPLs",
+// },
+// {
+//     til:"FATIGUE",
+//     imgsrc:"https://iwall365.com/iPhoneWallpaper/640x960/1310/Iron-Man-3-Robert-Downey-Jr_640x960_iPhone_4_wallpaper.jpg",
+//     slink:"https://www.youtube.com/watch?v=FEf412bSPLs",
+// },
+// ];
+
